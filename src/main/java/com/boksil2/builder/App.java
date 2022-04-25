90991f8f44a6138c8aad51d28be24d8b60572137
bro-joon <brojoon.m@gmail.com>
2022-04-26 00:30:07 +0900

    builder

---
+++ src/main/java/com/boksil2/builder/App.java
@@ -0,0 +1,13 @@
+package com.boksil2.builder;
+
+import java.time.LocalDate;
+
+public class App {
+    public static void main(String[] args) {
+
+        TourDirector director = new TourDirector(new DefaultTourBuilder());
+        TourPlan tourPlan = director.cancunTrip();
+        TourPlan tourPlan1 = director.longBeachTrip();
+
+    }
+}
